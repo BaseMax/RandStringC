8f74e45fcc8d53506cc37ed7ed7921acd8c73b92
Max Base <MaxBaseCode@Gmail.Com>
2019-03-05 21:02:31 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : RandString.C
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-05
+* @Released under : https://github.com/BaseMax/RandStringC/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/RandStringC
+*
+**/
